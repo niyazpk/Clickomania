beb7c87f882dc4823563817215d731b79734113f
Niyaz PK <niyazpk@gmail.com>
2013-02-16 20:58:39 +0530

    fix variable names

--- click.js
+++ click.js
@@ -1,56 +1,55 @@
 
-var game_matrix = [];
-var old_game_matrix = [];
+var gameMatrix, oldGameMatrix;
 
-var height = 500; // 500 px
-var cube_height = 28; // 28 px
-var cube_width = cube_height;
-var no_of_rows = Math.round(height / cube_height);
-var no_of_cols = 10;
+var height     = 500; // 500 px
+var cubeHeight = 28;  // 28 px
+var cubeWidth  = cubeHeight;
+var rowCount   = Math.round(height / cubeHeight);
+var colCount   = 10;
 
-function new_game() {
+function startNewGame() {
 
-	game_matrix = [];
-	old_game_matrix = [];
+	gameMatrix    = [];
+	oldGameMatrix = [];
 
-	for (i = 0; i < no_of_rows; i++) {
+	for (i = 0; i < rowCount; i++) {
 
 		// html += "<tr>";
 		cur_row = [];
 
-		for (j = 0; j < no_of_cols; j++) {
+		for (j = 0; j < colCount; j++) {
 			// Get random number between 1 and total number of supported cubes
 			var total_cubes = parseInt($('#difficulty_level').val(), 10);
 			var cur_index = Math.floor(1 + (Math.random() * total_cubes))
 
 			cur_row.push(cur_index);
-			
+
 		}
 
-		game_matrix.push(cur_row);
+		gameMatrix.push(cur_row);
 	}
 
-	old_game_matrix = game_matrix.slice(0);
-	console.log(old_game_matrix);
-	draw_from_game_matrix();
+	oldGameMatrix = gameMatrix.slice(0);
+	console.log(oldGameMatrix);
+	draw_from_gameMatrix();
 }
 
-function draw_from_game_matrix() {
+function draw_from_gameMatrix() {
 
 	var html = "<table>";
 
-	for (i = 0; i < no_of_rows; i++) {
+	for (i = 0; i < rowCount; i++) {
 
 		html += "<tr>";
-		cur_row = game_matrix[i];
+		cur_row = gameMatrix[i];
 
-		for (j = 0; j < no_of_cols; j++) {
+		for (j = 0; j < colCount; j++) {
 
 			var cur_index = cur_row[j]
 			var cube_class = "cube" + cur_index;
 
 			html += "<td class='cube " + cube_class + "' data-cube_index='" + cur_index + "' data-row='" + i + "' data-col='" + j + "'>&nbsp;</td>";
-			
+
 		}
 
 		html += "</tr>";
@@ -74,10 +73,10 @@ $('#game_area').on('click', '.cube', function() {
 	var neighbour_found = check_if_neighbour_exists(row, col, cube_index);
 
 	if (neighbour_found) {
-		// old_game_matrix = game_matrix.slice(0);
+		// oldGameMatrix = gameMatrix.slice(0);
 		mark_cube_to_remove(row, col);
 		cube_index_to_remove = cube_index;
-		process_cubes_to_remove();	
+		process_cubes_to_remove();
 	} else {
 		// Wrong - No cells
 	}
@@ -95,25 +94,25 @@ function check_if_neighbour_exists(row, col, cube_index) {
 	}
 
 	if (row > 0) {
-		if(game_matrix[row - 1][col] == cube_index) {
+		if(gameMatrix[row - 1][col] == cube_index) {
 			neighbour_found = true;
 		}
 	}
 
 	if (col > 0) {
-		if(game_matrix[row][col - 1] == cube_index) {
+		if(gameMatrix[row][col - 1] == cube_index) {
 			neighbour_found = true;
 		}
 	}
 
-	if (row < no_of_rows - 1) {
-		if(game_matrix[row + 1][col] == cube_index) {
+	if (row < rowCount - 1) {
+		if(gameMatrix[row + 1][col] == cube_index) {
 			neighbour_found = true;
 		}
 	}
 
-	if (col < no_of_cols - 1) {
-		if(game_matrix[row][col + 1] == cube_index) {
+	if (col < colCount - 1) {
+		if(gameMatrix[row][col + 1] == cube_index) {
 			neighbour_found = true;
 		}
 	}
@@ -125,14 +124,14 @@ function check_if_move_exists() {
 	valid_move_exists = false;
 	non_white_exists = false;
 
-	for (i = 0; i < no_of_rows; i++) {
-		for (j = 0; j < no_of_cols; j++) {
-			
-			if (game_matrix[i][j] != 0) {
+	for (i = 0; i < rowCount; i++) {
+		for (j = 0; j < colCount; j++) {
+
+			if (gameMatrix[i][j] != 0) {
 				non_white_exists = true;
 			}
 
-			if(check_if_neighbour_exists(i, j, game_matrix[i][j])) {
+			if(check_if_neighbour_exists(i, j, gameMatrix[i][j])) {
 				valid_move_exists = true;
 				break;
 			}
@@ -172,70 +171,70 @@ function process_cubes_to_remove() {
 		var col = cur_cube['col'];
 
 		if (row > 0) {
-			if(game_matrix[row - 1][col] == cube_index) {
+			if(gameMatrix[row - 1][col] == cube_index) {
 				mark_cube_to_remove(row - 1, col);
 			}
 		}
 
 		if (col > 0) {
-			if(game_matrix[row][col - 1] == cube_index) {
+			if(gameMatrix[row][col - 1] == cube_index) {
 				mark_cube_to_remove(row, col - 1);
 			}
 		}
 
-		if (row < no_of_rows - 1) {
-			if(game_matrix[row + 1][col] == cube_index) {
+		if (row < rowCount - 1) {
+			if(gameMatrix[row + 1][col] == cube_index) {
 				mark_cube_to_remove(row + 1, col);
 			}
 		}
 
-		if (col < no_of_cols - 1) {
-			if(game_matrix[row][col + 1] == cube_index) {
+		if (col < colCount - 1) {
+			if(gameMatrix[row][col + 1] == cube_index) {
 				mark_cube_to_remove(row, col + 1);
 			}
 		}
 
-		game_matrix[row][col] = -1;
+		gameMatrix[row][col] = -1;
 	}
 
 	remove_marked_cubes();
 }
 
 function remove_marked_cubes() {
-	for (i = 0; i < no_of_cols; i++) {
-		for (j = no_of_rows - 1; j >= 0; j--) {
-			if (game_matrix[j][i] == -1 && j != 0) {
+	for (i = 0; i < colCount; i++) {
+		for (j = rowCount - 1; j >= 0; j--) {
+			if (gameMatrix[j][i] == -1 && j != 0) {
 
 				for (k = j; k >= 0; k--) {
 					if (k != 0) {
-						game_matrix[k][i] = game_matrix[k - 1][i]
+						gameMatrix[k][i] = gameMatrix[k - 1][i]
 					} else {
-						game_matrix[k][i] = 0;
+						gameMatrix[k][i] = 0;
 					}
 				}
 
 				j++;
 
-			} else if (game_matrix[j][i] == -1 && j == 0) {
-				game_matrix[j][i] = 0;
+			} else if (gameMatrix[j][i] == -1 && j == 0) {
+				gameMatrix[j][i] = 0;
 			}
 		}
 	}
 
 	// Check the last row
-	for (j = 0; j < no_of_cols - 1; j++) {
-		if (game_matrix[no_of_rows - 1][j] == 0) {
+	for (j = 0; j < colCount - 1; j++) {
+		if (gameMatrix[rowCount - 1][j] == 0) {
 			// Shift all to the left
-			for (m = j; m < no_of_cols - 1; m++) {
-				for (n = 0; n < no_of_rows; n++) {
-					game_matrix[n][m] = game_matrix[n][m + 1];
-					game_matrix[n][m + 1] = 0;
+			for (m = j; m < colCount - 1; m++) {
+				for (n = 0; n < rowCount; n++) {
+					gameMatrix[n][m] = gameMatrix[n][m + 1];
+					gameMatrix[n][m + 1] = 0;
 				}
 			}
 		}
 	}
 
-	draw_from_game_matrix();
+	draw_from_gameMatrix();
 }
 
 function mark_cube_to_remove(row, col) {
@@ -243,16 +242,16 @@ function mark_cube_to_remove(row, col) {
 }
 
 $(document).ready(function() {
-	new_game();
+	startNewGame();
 });
 
 $('#btn_undo').on('click', function() {
-	game_matrix = old_game_matrix.slice(0);
-	draw_from_game_matrix();
+	gameMatrix = oldGameMatrix.slice(0);
+	draw_from_gameMatrix();
 });
 
 $('#btn_new_game').on('click', function() {
-	new_game();
+	startNewGame();
 });
 
 $('.show_help').on('click', function() {
